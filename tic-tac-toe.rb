9b38d85b43a711c3b8cce0ef1836fb4411375c1e
Hauptmannhuy <dindago2324@gmail.com>
2023-10-11 18:48:42 +0300

    completed implementation of player choose logic

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,15 +1,25 @@
 class Game
-  @@board =' 
-  1 | 2 | 3
- ---+---+---
-  3 | 4 | 5
- ---+---+---
-  6 | 7 | 9
- ---+---+--- 
- '
-  def self.show_board 
-    @@board
+  @@numbers = {1 =>1, 2 => 2, 3 =>3, 4 =>4, 5 =>5, 6 =>6, 7 =>7, 8 =>8, 9 =>9}
+  def self.numbers
+    @@numbers
   end
+  def self.show_board ="
+  #{Game.numbers[1]} | #{Game.numbers[2]} | #{Game.numbers[3]}
+ ---+---+---
+  #{Game.numbers[4]} | #{Game.numbers[5]} | #{Game.numbers[6]}
+ ---+---+---
+  #{Game.numbers[7]} | #{Game.numbers[8]} | #{Game.numbers[9]}
+ ---+---+--- 
+  " 
+end
+  def player_move
+    puts "Make your move!"
+     input = gets.chomp.to_i
+    change_board(input)
+end
+def change_board(input)
+  Game.numbers[input] = 'X'
+  puts Game.show_board
 end
 
 class Player
@@ -23,21 +33,29 @@ class Player
     @@player_count+=1
   end
   def Player.choose_name_marker
+
     puts "What is the name of player #{@@player_count}?"
     input_name = gets.chomp
     if @@players.any? && input_name == Player.players[0].name
+      while input_name == Player.players[0].name do
       puts "Your name shouldn't be #{Player.players[0].name}"
-    end
-    puts 'What game marker would you like to use? (Select 1 letter or special character)'
-    input_marker = gets.chomp
-    if input_marker.length != 1 
-      while input_marker.length != 1 do
-        puts 'Wrong input, please, try again'
-        input_marker = gets.chomp
+      input_name = gets.chomp
       end
     end
+
+    puts 'What game marker would you like to use? (Select 1 letter or special character)'
+    input_marker = gets.chomp
+    while input_marker.length != 1 || players.any? {|player| player.marker == input_marker}
+      if input_marker.length != 1 
+        puts "Your mark should be exactly one character"
+      else
+        puts "Your marker shouldn't be #{input_marker}. Please choose another marker."
+    end
+    input_marker = gets.chomp
+  end
     Player.new(input_name,input_marker)
 end
+
 def self.players
     return @@players
   end
@@ -47,5 +65,5 @@ puts p1 = Player.choose_name_marker
 puts p2 = Player.choose_name_marker
 p Player.players
 
-
-
+puts Game.show_board
+player_move
