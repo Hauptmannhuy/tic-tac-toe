5ea59378035cd6d822da51b7381054787d0c30fb
Hauptmannhuy <dindago2324@gmail.com>
2023-10-11 15:49:22 +0300

    reworked implementation of player creation

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -13,29 +13,39 @@ class Game
 end
 
 class Player
-  @@players = 1
+  @@player_count = 1
+  @@players = []
   attr_reader :name, :marker
   def initialize(name, marker)
     @name = name
     @marker = marker
-    @@players+=1
+    @@players << self
+    @@player_count+=1
   end
   def Player.choose_name_marker
-    puts "What is the name of player #{@@players}?"
+    puts "What is the name of player #{@@player_count}?"
     input_name = gets.chomp
+    if @@players.any? && input_name == Player.players[0].name
+      puts "Your name shouldn't be #{Player.players[0].name}"
+    end
     puts 'What game marker would you like to use? (Select 1 letter or special character)'
     input_marker = gets.chomp
     if input_marker.length != 1 
       while input_marker.length != 1 do
         puts 'Wrong input, please, try again'
         input_marker = gets.chomp
-        
       end
     end
     Player.new(input_name,input_marker)
 end
+def self.players
+    return @@players
+  end
 end
+
 puts p1 = Player.choose_name_marker
 puts p2 = Player.choose_name_marker
+p Player.players
+
 
 
